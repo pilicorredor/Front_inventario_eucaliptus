c0b74005b7a997a22436a853a618a6392388f2dc
pilicorredor <pilicorredor111@gmail.com>
2024-09-24 21:30:26 -0500

    add logo in login page

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './LoginForm.css';
 import { FaUser } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import logo from "../Assets/logo2.png";
 
 const LoginForm = () => {
   return (
     <div className='wrapper'>
         <form action="">
+            <img src={logo} alt="Logo de la Empresa" className="logo" />
             <h1>¡Bienvenido!</h1>
             <div className='input-box'>
                 <input type="text" placeholder='Nombre de Usuario' required/>
@@ -20,7 +22,9 @@ const LoginForm = () => {
                 <label><input type='checkbox'/>Recuerdame</label>
                 <a href='#'>¿Olvidaste tu contraseña?</a>
             </div>
-            <button type='submit'>Entrar</button>
+            <div className='button-container'>
+                <button type='submit'>Entrar</button>
+            </div>
         </form>
     </div>
   )
